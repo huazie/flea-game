5f3b45e679bbce28128d196163387bf81e1fac06
Huazie <huazie.lgh@gmail.com>
2025-06-19 21:15:37 +0800

    Mod: 支持难度切换 、支持手机端滑动或者方向键控制贪吃蛇方向

--- snake/js/storage.js
+++ snake/js/storage.js
@@ -1,6 +1,7 @@
 // 处理游戏数据的本地存储
 class GameStorage {
     static BEST_SCORE_KEY = 'max_score';
+    static DIFFICULTY_KEY = 'snake_difficulty';
 
     static getBestScore() {
         return parseInt(localStorage.getItem(this.BEST_SCORE_KEY)) || 0;
@@ -12,6 +13,14 @@ class GameStorage {
 
     static clearBestScore() {
         localStorage.removeItem(this.BEST_SCORE_KEY);
+    }
+
+    static getDifficulty() {
+        return localStorage.getItem(this.DIFFICULTY_KEY) || 'normal';
+    }
+
+    static saveDifficulty(difficulty) {
+        localStorage.setItem(this.DIFFICULTY_KEY, difficulty);
     }
 }
 
